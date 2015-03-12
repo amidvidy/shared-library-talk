db86d47b51c07dce20acfbd2da747bebb109e4ef
Adam Midvidy <amidvidy@gmail.com>
2015-03-12 09:28:58 -0400

    fix verbose

--- v0-first-attempt/app/build.sh
+++ v0-first-attempt/app/build.sh
@@ -2,10 +2,9 @@
 
 set -o errexit
 
-set -o verbose
-
 echo "building application..."
+set -o verbose
 c++ app.cpp -Wall --std=c++1y -o app -I../lib -L../lib -Wl,-rpath='$ORIGIN/../lib' -lfactorial
+set +o verbose
 echo "...done!"
 
-set +o verbose
